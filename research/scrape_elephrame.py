2721f49efc2716ec31a873fcfe55ef97ddc9a9e2
Joe Colasurdo <jecolasurdo@gmail.com>
2020-06-16 03:13:51 -0700

    Raw elephrame data scraped. Not perfect, but sufficient.

--- research/scrape_elephrame.py
+++ research/scrape_elephrame.py
@@ -1,49 +1,63 @@
 from selenium import webdriver
 import time
 import hashlib
+import os
 
+
+NUM_PAGES = 165
 BASE_URL = "https://elephrame.com/textbook/BLM/chart"
-LAST_PAGE = 165
+RAW_DIRECTORY = "research/raw"
+PAGE_XPATH = "//div[@id='blm-results']/div/ul/li[3]/input"
+NEXT_XPATH = "//div[@id='blm-results']/div/ul/li[4]"
 
-browser = webdriver.Chrome()
-browser.get(BASE_URL)
-page_num = 1
-previous_num = 0
-wait = 1
-successes = 0
-previous_hash = hash("")
 
-while page_num <= LAST_PAGE:
-    try:
-        time.sleep(wait)
+def scrape():
+    page_num = 1
+    previous_num = 0
+    wait = 1
+    successes = 0
+    previous_hash = hash("")
+    cwd = os.getcwd()
 
-        page_input = browser.find_elements_by_xpath(
-            "//div[@id='blm-results']/div/ul/li[3]/input")[0]
-        page_num = int(page_input.get_attribute("value"))
-        if page_num == previous_num:
-            raise Exception("same page number")
+    driver = webdriver.Chrome()
+    driver.get(BASE_URL)
+    while page_num < NUM_PAGES:
+        try:
+            time.sleep(wait)
 
-        html_source = browser.page_source
-        current_hash = hash(html_source)
-        if current_hash == previous_hash:
-            raise Exception("duplicate page")
+            page_input = driver.find_elements_by_xpath(PAGE_XPATH)[0]
+            page_num = int(page_input.get_attribute("value"))
+            if page_num == previous_num:
+                raise Exception("same page number")
 
-        print(page_num, current_hash)
-        previous_hash = current_hash
+            html_source = driver.page_source
+            current_hash = _hash(html_source)
+            if current_hash == previous_hash:
+                raise Exception("duplicate page")
+            previous_hash = current_hash
 
-        next_page_button = browser.find_element_by_xpath(
-            "//div[@id='blm-results']/div/ul/li[4]")
-        next_page_button.click()
+            filename = "{}/{}/page_{}.html".format(cwd, RAW_DIRECTORY, page_num)
+            print("writing", filename)
+            with open(filename, "w") as f:
+                f.write(html_source)
 
-        successes = successes + 1
-        if successes % 10 == 0 and wait > 1:
-            wait = wait - 1
-            print("decrement wait", wait)
-    except Exception as e:
-        wait = wait + 2
-        print("increment wait", wait)
-        print(page_num, e)
-browser.quit()
+            next_page_button = driver.find_element_by_xpath(NEXT_XPATH)
+            next_page_button.click()
 
-def hash(s):
+            successes = successes + 1
+            if successes % 10 == 0 and wait > 1:
+                wait = wait - 1
+                print("decrement wait", wait)
+        except Exception as e:
+            wait = wait + 1
+            print("increment wait", wait)
+            print(page_num, e)
+    driver.quit()
+
+
+def _hash(s):
     return hashlib.md5(s.encode('utf-8')).hexdigest()
+
+
+if __name__ == "__main__":
+    scrape()
