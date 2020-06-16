4b090a9730587978271239cb03c8e2a93da94db9
Joe Colasurdo <jecolasurdo@gmail.com>
2020-06-16 14:54:49 -0700

    Tidying up the scraper.

--- research/scrape_elephrame.py
+++ research/scrape_elephrame.py
@@ -4,54 +4,105 @@ import hashlib
 import os
 
 
-NUM_PAGES = 165
 BASE_URL = "https://elephrame.com/textbook/BLM/chart"
-RAW_DIRECTORY = "research/raw"
 PAGE_XPATH = "//div[@id='blm-results']/div/ul/li[3]/input"
 NEXT_XPATH = "//div[@id='blm-results']/div/ul/li[4]"
+MAX_FAILURES = 10
+SUCCESS_DOWNGRADE_INTERVAL = 5
+MINIMUM_LOAD_WAIT_SECS = 1
+WAIT_DECREMENT_INTEVAL = 1
+WAIT_INCREMENT_INTEVAL = 1
 
 
-def scrape():
+def scrape(raw_directory="research/raw", num_pages=165):
+    """Scrapes raw HTML BLM data for each page at https://elephrame.com/textbook/BLM/chart
+    and saves the HTML for each page to a local directory.
+
+    Parameters
+    ----------
+    raw_directory : str
+        The directory into which the raw html files will be saved.
+
+    num_pages : int
+        The number of pages to scrape.
+
+    Raises
+    ------
+    Exception
+        The max number of consecutive failures (MAX_FAILURES) has been reached.
+
+    Notes
+    -----
+    Since the main table in the scraped page loads asyncronously, it can be
+    difficult to know that the content has been fully rendered prior to automating
+    the click event to load the next page of the table. The function tries
+    to cope with this by checking that the page content has changed since the
+    last time it loaded, and by dynamically adjusting a wait timer to give the
+    components sufficient time to load before processing. This approach
+    is imperfect, but seems to be minimally sufficient to meet current needs.
+
+    Since the waiting mechanisms currently employed here are fairly crude, the
+    function may accidentally skip some pages as it moves along. This usually
+    occurs if the function tries to submit a multiple click events too qucicky,
+    in which case, the function thinks it had time to process a page that it
+    never actually processed. The function will recover from this and move on to
+    the next page cleanly, but nothing is done to try and recover the skipped
+    page.
+
+    This function currently runs chrome as the driver, and does not run 
+    headless. The function is fragile to the size of the browser window that is 
+    opened. Changing the browser window can cause the DOM to restructure, which
+    can cause the XPATH assumptions to become invalid. Not worth fixing at the
+    moment, but be warned.
+    """
+
     page_num = 1
     previous_num = 0
-    wait = 1
-    successes = 0
+    wait = MINIMUM_LOAD_WAIT_SECS
+    consecutive_successes = 0
+    consecutive_failures = 0
     previous_hash = hash("")
     cwd = os.getcwd()
 
     driver = webdriver.Chrome()
     driver.get(BASE_URL)
-    while page_num < NUM_PAGES:
+    while page_num < num_pages:
         try:
             time.sleep(wait)
 
             page_input = driver.find_elements_by_xpath(PAGE_XPATH)[0]
             page_num = int(page_input.get_attribute("value"))
             if page_num == previous_num:
-                raise Exception("same page number")
+                raise Exception("Page number hasn't changed yet.")
 
             html_source = driver.page_source
             current_hash = _hash(html_source)
             if current_hash == previous_hash:
-                raise Exception("duplicate page")
+                raise Exception("Page content hasn't changed yet.")
             previous_hash = current_hash
 
-            filename = "{}/{}/page_{}.html".format(cwd, RAW_DIRECTORY, page_num)
+            filename = "{}/{}/page_{}.html".format(cwd,
+                                                   raw_directory, page_num)
             print("writing", filename)
             with open(filename, "w") as f:
                 f.write(html_source)
 
-            next_page_button = driver.find_element_by_xpath(NEXT_XPATH)
-            next_page_button.click()
+            driver.find_element_by_xpath(NEXT_XPATH).click()
 
-            successes = successes + 1
-            if successes % 10 == 0 and wait > 1:
-                wait = wait - 1
-                print("decrement wait", wait)
+            consecutive_successes = consecutive_successes + 1
+            consecutive_failures = 0
+            if consecutive_successes >= SUCCESS_DOWNGRADE_INTERVAL and wait - WAIT_DECREMENT_INTEVAL >= MINIMUM_LOAD_WAIT_SECS:
+                consecutive_successes = 0
+                wait = wait - WAIT_DECREMENT_INTEVAL
+                print("Decrementing wait to", wait)
         except Exception as e:
-            wait = wait + 1
-            print("increment wait", wait)
             print(page_num, e)
+            consecutive_failures = consecutive_failures + 1
+            consecutive_successes = 0
+            if consecutive_failures >= MAX_FAILURES:
+                raise Exception("Reached max number of consecutive failures.")
+            wait = wait + WAIT_INCREMENT_INTEVAL
+            print("Incrementing wait to", wait)
     driver.quit()
 
 
